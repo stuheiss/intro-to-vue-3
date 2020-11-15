349a8072a89f1dc10764216386bfde7e5e23aeda
Stu Heiss <sheiss@highlandsolutions.com>
2020-11-15 14:05:18 -0600

    List Rendering

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,13 @@
             <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
             <p v-else>Out of Stock</p>
             <p v-if="onSale">On Sale</p>
+            <ul>
+              <li v-for="detail in details" :key="detail.id">{{ detail }}</li>
+            </ul>
+            <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
+            <ul>
+              <li v-for="size in sizes" :key="size.id">{{ size }}</li>
+            </ul>
           </div>
         </div>
       </div>
@@ -28,6 +35,12 @@ export default {
       image: './assets/images/socks_green.jpg',
       inventory: 100,
       onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id:2234, color:'green' },
+        { id:2235, color:'blue' },
+      ],
+      sizes: ['small', 'medium', 'large'],
     };
   }
 }
